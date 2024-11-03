73121cc583a4382c935dd80acf7796a94b3320d7
M.Arshad <arshadm1512@gmail.com>
2024-11-03 19:02:42 +0530

    in commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let about = document.getElementsByClassName("about")[0];
+about.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+let contact = document.getElementsByClassName("contact")[0];
+contact.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("contact").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+let services = document.getElementsByClassName("services")[0];
+services.addEventListener("click", function(e){
+    e.preventDefault();
+    document.getElementById("services").scrollIntoView({
+        behavior:"smooth"
+    });
+});
+
+let project = document.getElementsByClassName("project")[0];
+project.addEventListener("click", function(e){
+    e.preventDefault();
+    document.getElementById("project").scrollIntoView({
+        behavior:"smooth"
+    });
+});
+
+
